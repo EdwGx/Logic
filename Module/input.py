3baedf3dbc9d7638ee29d277460fd00405ff67ba
EdwGx <edguo98@gmail.com>
2013-12-23 12:53:47 -0500

    Add Input class and switch image(change later)

---
+++ Module/input.py
@@ -0,0 +1,44 @@
+from Gate import logicGate,Port
+class Input(logicGate):
+    def __init__(self):
+        logicGate.__init__(self,8)
+        self.port.append(Port((32,30),True)) #OUTPUT
+        self.port.append(Port((40,30),False))
+        self.port[1].conn_wire = False
+        self.margin = 10
+        self.draw_image()
+
+    def update(self):
+        port[0].update()
+
+
+    def port_pos(self,port_id):
+        return((self.rect.x + self.port[port_id].position[0]),
+               (self.rect.y + self.port[port_id].position[1]))
+
+    def number_of_ports(self):
+        return 1
+
+    def change_status(self):
+        self.status = not(self.status)
+        for port in self.port:
+            port.update_status(self.status)
+    
+    def click_res(self,rel_pos):
+        if rel_pos[0] > self.margin and rel_pos[0] < (self.rect.width - self.margin):
+            if rel_pos[1] > self.margin and rel_pos[0] < (self.rect.height - self.margin):
+                
+            
+
+    def move_update(self):
+        for port in self.port:
+            port.update_req()
+
+    def draw_image(self):
+        if self.status:
+            self.image = pygame.image.load(os.path.join('Module','Resources','switch_on.png'))
+            self.rect = self.image.get_rect()
+        else:
+            self.image = pygame.image.load(os.path.join('Module','Resources','switch_off.png'))
+            self.rect = self.image.get_rect()
+        
